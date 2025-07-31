2c660a14ebb60d36f71b5571b2ad95260aecdd60
tiberiuiancu <tiberiu.iancu@gmail.com>
2025-07-31 21:06:43 +0200

    backward pass dx

--- hypll/kernels/fc_kernel.py
+++ hypll/kernels/fc_kernel.py
@@ -1,8 +1,4 @@
-import os
 import math
-import triton.testing
-
-from hypll.manifolds.poincare_ball.math.linalg import poincare_fully_connected
 
 import torch
 import triton
@@ -176,82 +172,160 @@ def poincare_fully_connected_triton(x, z, r=None, c=1.0):
     return numerator / denominator[:, None]
 
 
-def poincare_fc_ref(x, z, bias, c):
+def poincare_fc_fwd_ref(x, z, bias, c):
+    """
+    For my own reference; it's the same as the hypll implementation, but in a single function.
+    Args:
+        x: [B, K] input tensor (fp32)
+        z: [K, M] weight tensor (fp32)
+        bias: [M] bias tensor or None (fp32)
+        c: curvature (float)
+    Returns:
+        Output tensor [B, M] (fp32)
+    """
     c_sqrt = math.sqrt(c)
-    lam = 2 / (1 - c * x.pow(2).sum(-1, keepdim=True))
-    z_norm = z.norm(dim=0).clamp_min(1e-15)
+    lam = 2 / (1 - c * x.pow(2).sum(-1, keepdim=True))  # shape [B, 1]
+    z_norm = z.norm(dim=0).clamp_min(1e-15)  # shape [M]
 
-    inner = c_sqrt * lam / z_norm * (x @ z)
+    inner_tmp = c_sqrt * lam / z_norm * (x @ z)  # shape [B, M]
     if bias is not None:
         two_cs_r = 2.0 * c_sqrt * bias
-        inner = inner * torch.cosh(two_cs_r) - (lam - 1) * torch.sinh(two_cs_r)
+        inner = inner_tmp * torch.cosh(two_cs_r) - (lam - 1) * torch.sinh(two_cs_r)
+    else:
+        inner = inner_tmp
 
     v = 2 * z_norm * torch.asinh(inner)
-    y = torch.sinh(v) / c_sqrt
-    numerator = y
-    denominator = 1 + torch.sqrt(1 + c * y.pow(2).sum(-1, keepdim=True))  # shape [B, 1]
-    return numerator / denominator
+    num = torch.sinh(v) / c_sqrt
+    den = 1 + torch.sqrt(1 + c * num.pow(2).sum(-1, keepdim=True))  # shape [B, 1]
+    return num / den
 
 
-@triton.testing.perf_report(
-    triton.testing.Benchmark(
-        x_names=["B", "M", "K"],
-        x_vals=[128 * i for i in range(1, 17)],  # B = M = K = 128, 256, ..., 2048
-        line_arg="provider",
-        line_vals=["triton", "torch", "compile", "matmul"],
-        line_names=["Triton", "PyTorch", "Compiled PyTorch", "Euclidean"],
-        styles=[("red", "-"), ("blue", "--"), ("green", "-."), ("orange", ":")],
-        ylabel="TFLOPS",
-        plot_name="poincare_fc-performance",
-        args={"c": 0.1},
-    )
-)
-def bench(B, M, K, c, provider):
-    B, M, K = int(B), int(M), int(K)
-    torch.manual_seed(42)
-    device = "cuda"
-    x = torch.randn(B, K, device=device, dtype=torch.float32)
-    z = torch.randn(K, M, device=device, dtype=torch.float32)
-    r = torch.randn(M, device=device, dtype=torch.float32)
+@triton.jit
+def _dnum_dx(
+    x,  # [BLOCK_K] - loaded slice of input
+    v,  # [BLOCK_M]
+    z,  # [BLOCK_K, BLOCK_M]
+    inner,  # [BLOCK_M]
+    twocsr,  # [BLOCK_M]
+    lam,  # scalar
+    HAS_BIAS: tl.constexpr,
+):
+    # broadcast all vectors
+    x = x[:, None]
+    v = v[None, :]
+    inner = inner[None, :]
+    twocsr = twocsr[None, :]
 
-    if provider == "triton":
-        fn = lambda: poincare_fully_connected_triton(x, z, r, c)
-    elif provider == "torch":
-        fn = lambda: poincare_fc_ref(x, z, r, c)
-    elif provider == "compile":
-        compiled = torch.compile(poincare_fc_ref)
-        fn = lambda: compiled(x, z, r, c)
-    elif provider == "matmul":
-        fn = lambda: x @ z
-    else:
-        raise ValueError(provider)
+    inner_sq = inner * inner
+    x_sq = x * x
 
-    ms = triton.testing.do_bench(fn)
-    # ms is milliseconds, so convert to seconds for TFLOPS calculation
-    seconds = ms / 1e3
-    tflops = (2 * B * M * K) / (seconds * 1e12)
-    return tflops
+    y = cosh(v) * 2 / tl.sqrt(1 + inner_sq) * z * lam * (1 + c * lam * x_sq)
+    if HAS_BIAS:
+        sinh_twocsr = sinh(twocsr)
+        y = y * cosh(twocsr) - sinh_twocsr * c * lam * lam * x + sinh_twocsr
+
+    return y
 
 
-def test_reference():
-    """Run a simple correctness test."""
-    B, K, M = 256, 128, 64
-    c = torch.tensor(0.1, dtype=torch.float32).cuda()
-    x = torch.randn(B, K, dtype=torch.float32).cuda()
-    z = torch.randn(K, M, dtype=torch.float32).cuda()
-    r = torch.randn(M, dtype=torch.float32).cuda()
+@triton.jit
+def _poincare_fc_bwd_dx_kernel(
+    x_ptr,  # [B, K] - input
+    z_ptr,  # [K, M] - weights
+    dout_ptr,  # [B, M] - output gradient
+    num_ptr,  # [B, M] - cache of "num" from fwd pass
+    v_ptr,  # [B, M] - cache of "v" from fwd pass
+    inner_ptr,  # [B, M] - cache of "inner" from fwd pass
+    lam_ptr,  # [B] - cache of lambda from fwd pass
+    den_ptr,  # [B] - cache of den from fwd pass
+    twocsr_ptr,  # [M] - 2 * c_sqrt * bias, or dummy if no bias
+    c,  # curvature, scalar
+    out_ptr,  # [K] - pointer to write output to
+    x_stride_b: tl.constexpr,
+    z_stride_k: tl.constexpr,
+    dout_stride_b: tl.constexpr,
+    num_stride_b: tl.constexpr,
+    v_stride_b: tl.constexpr,
+    inner_stride_b: tl.constexpr,
+    BLOCK_K: tl.constexpr,
+    BLOCK_M: tl.constexpr,
+    K: tl.constexpr,
+    M: tl.constexpr,
+    B: tl.constexpr,
+    HAS_BIAS: tl.constexpr,
+):
+    b = tl.program_id(0)
+    k = tl.program_id(1) * BLOCK_K
 
-    y = poincare_fully_connected(x, z, r, c)
-    y_ref = poincare_fc_ref(x, z, r, c)
-    y_triton = poincare_fully_connected_triton(x, z, r, c)
+    # offset pointers to current row
+    x_ptr += b * x_stride_b
+    dout_ptr += b * dout_stride_b
+    num_ptr += b * num_stride_b
+    v_ptr += b * v_stride_b
+    inner_ptr += b * inner_stride_b
 
-    assert torch.allclose(y, y_ref, atol=1e-3), (y - y_ref).abs().max().item()
-    assert torch.allclose(y, y_triton, atol=1e-3), (y - y_triton).abs().max().item()
+    # load row of x in memory for the entire kernel execution
+    offs_k = k + tl.arange(0, BLOCK_K)
+    mask_k = offs_k < K
+    x = tl.load(x_ptr + offs_k, mask=mask_k)
 
+    # load lambda and denominator value of current row
+    lam = tl.load(lam_ptr + b, mask=b < B)
+    den = tl.load(den_ptr + b, mask=b < B)
 
-if __name__ == "__main__":
-    test_reference()
+    ###### STAGE 1: precompute sum for derivative of den w.r.t. x
+    offs_m = tl.arange(0, BLOCK_M)
+    sum_buf = tl.zeros([BLOCK_K], dtype=tl.float32)
+    for m in range(0, M, BLOCK_M):
+        # calculate z offsets
+        offs_z = offs_k[:, None] * z_stride_k + offs_m[None, :]
+        mask_z = (offs_k[:, None] < K) & (offs_m[None, :] < M)
 
-    # Run benchmarks and save plots
-    os.makedirs("plots", exist_ok=True)
-    bench.run(show_plots=False, print_data=True, save_path="./plots/poincare_fc_performance_B")
+        # load data
+        mask_m = offs < M
+        num = tl.load(num_ptr + offs_m, mask=mask_m)
+        v = tl.load(v_ptr + offs_m, mask=mask_m)
+        inner = tl.load(inner_ptr + offs_m, mask=mask_m)
+        z = tl.load(z_ptr + offs_z, mask=mask_z)
+        twocsr = num
+        if HAS_BIAS:
+            twocsr = tl.load(twocsr_ptr + offs_m, mask=mask_m)
+
+        # calculate derivative of num w.r.t. x
+        d = _dnum_dx(x, v, z, inner, twocsr, lam, HAS_BIAS)  # shape [BLOCK_K, BLOCK_M]
+
+        # aggregate the sum over M dimension, multiply by num
+        sum_buf += tl.sum(d, axis=1) * num
+
+        # increment offsets
+        offs_m += BLOCK_M
+
+    ###### STAGE 2: compute the sum over dout M dimension
+    # cache some values
+    den_inv = 1 / den
+    c_den_1 = c / (den - 1)
+
+    offs_m = tl.arange(0, BLOCK_M)
+    out_buf = tl.zeros([BLOCK_K], dtype=tf.float32)
+    for m in range(0, M, BLOCK_M):
+        # calculate z offsets
+        offs_z = offs_k[:, None] * z_stride_k + offs_m[None, :]
+        mask_z = (offs_k[:, None] < K) & (offs_m[None, :] < M)
+
+        # load data
+        mask_m = offs < M
+        dout = tl.load(dout_ptr + offs_m, mask=mask_m)
+        num = tl.load(num_ptr + offs_m, mask=mask_m)
+        v = tl.load(v_ptr + offs_m, mask=mask_m)
+        inner = tl.load(inner_ptr + offs_m, mask=mask_m)
+        z = tl.load(z_ptr + offs_z, mask=mask_z)
+        twocsr = num
+        if HAS_BIAS:
+            twocsr = tl.load(twocsr_ptr + offs_m, mask=mask_m)
+
+        d = _dnum_dx(x, v, z, inner, twocsr, lam, HAS_BIAS)
+        dy_dx = den_inv * d - c_den_1 * num[None, :] * sum_buf[:, None]
+
+        # aggregate sum over M dim, multiply by dout
+        out_buf += dy_dx * dout
+
+    tl.store(out_ptr + offs_k, out_buf, mask=mask_k)
