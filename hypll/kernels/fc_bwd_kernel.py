fix derivatives code

hypll/kernels/fc_bwd_kernel.py
@@ -122,7 +122,7 @@ def _poincare_fc_bwd_kernel(
 def _dL_dY(
     # inputs
     dout_ptr,
-    Y_ptr,
+    dout_y_sum_ptr,
     num_ptr,
     yn_ptr,
     max_norm,
@@ -143,7 +143,6 @@ def _dL_dY(
     pid_m = tl.program_id(1)
 
     dout_ptr += dout_stride_b * pid_b
-    Y_ptr += Y_stride_b * pid_b
     num_ptr += num_stride_b * pid_b
     T1_ptr += T1_stride_b * pid_b
 
@@ -157,9 +156,9 @@ def _dL_dY(
     if yn < max_norm:
         T1 = dout
     else:
-        Y = tl.load(Y_ptr + offs_m, mask=mask_m, other=0.0)
+        dout_y_sum = tl.load(dout_y_sum_ptr + pid_b)
         yni = 1 / yn
-        T1 = dout * max_norm * yni * (1 - Y * Y * yni * yni)
+        T1 = max_norm * yni * (dout - yni * yni * dout_y_sum)
 
     tl.atomic_add(T1_num_ptr + pid_b, tl.sum(num * T1))
     tl.store(T1_ptr + offs_m, T1, mask=mask_m)
@@ -178,10 +177,11 @@ def poincare_fc_bwd_triton(dout, Y, X, Z, XZ, zn, b, lam, num, den, yn, max_norm
     # get dL_dY
     T1 = torch.empty_like(XZ)
     T1_num = torch.zeros_like(lam)
+    dout_y_sum = torch.einsum("ij,ij->i", dout, Y)
     grid = lambda meta: (B, triton.cdiv(M, meta["BLOCK_M"]))
     _dL_dY[grid](
         dout,
-        Y,
+        dout_y_sum,
         num,
         yn,
         max_norm,