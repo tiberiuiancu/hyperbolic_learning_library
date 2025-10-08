f752c6e1b76b42173065824cec1b2c2d845af5e6
tiberiuiancu <tiberiu.iancu@gmail.com>
2025-10-08 12:04:06 +0200

    fix arguments

--- hypll/kernels/fc_layer.py
+++ hypll/kernels/fc_layer.py
@@ -12,7 +12,6 @@ class FastPoincareFC(torch.autograd.Function):
         r: torch.Tensor = None,
         c: float = 1.0,
         dim: int = -1,
-        backend: str = "triton",
     ):
         x = x.movedim(source=dim, destination=-1)
         py, (y, x, z, xz, zn, b, lam, num, den, yn, max_norm, c, cs) = (
@@ -24,17 +23,16 @@ class FastPoincareFC(torch.autograd.Function):
         ctx.max_norm = max_norm
         ctx.dim = dim
         ctx.has_bias = r is not None
-        ctx.backend = backend
         return py.movedim(source=-1, destination=dim)
 
     @staticmethod
     def backward(ctx, dout):
         y, x, z, xz, zn, b, lam, num, den, yn = ctx.saved_tensors
         dx, dz, dr = poincare_fc_bwd_triton(
-            dout, y, x, z, xz, zn, b, lam, num, den, yn, ctx.max_norm, ctx.c, ctx.cs, ctx.backend
+            dout, y, x, z, xz, zn, b, lam, num, den, yn, ctx.max_norm, ctx.c, ctx.cs
         )
 
         dx = dx.movedim(source=-1, destination=ctx.dim)
         dr = dr if ctx.has_bias else None
 
-        return dx, dz, dr, None, None, None
+        return dx, dz, dr, None, None
