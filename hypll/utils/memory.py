f1175cb4dcd73329e0bf4bf289a86743213a876c
tiberiuiancu <tiberiu.iancu@gmail.com>
2025-10-09 10:38:52 +0200

    remove zero async from memory pool

--- hypll/utils/memory.py
+++ hypll/utils/memory.py
@@ -6,26 +6,17 @@ class GPUMemoryPool:
         self.device = device
         self.buffers = {}
         self.stream = torch.cuda.Stream(device=self.device)
-        self.zero_buffs = []
 
-    def alloc(self, shape, dtype=torch.float32, zero_buf=False):
-        buf = torch.empty(shape, dtype=dtype, device=self.device)
-        if zero_buf:
-            self.zero_buffs.append(buf)
-        return buf
+    def alloc(self, shape, dtype=torch.float32):
+        return torch.empty(shape, dtype=dtype, device=self.device)
 
-    def get_shared(self, name, shape, dtype=torch.float32, zero_buf=False):
+    def get_shared(self, name, shape, dtype=torch.float32):
         key = (name, shape, dtype)
         buf = self.buffers.get(key)
         if buf is None or buf.shape != shape or buf.dtype != dtype:
-            buf = self.alloc(shape, dtype, zero_buf=zero_buf)
+            buf = self.alloc(shape, dtype)
             self.buffers[key] = buf
         return buf
-
-    def zero_async(self):
-        with torch.cuda.stream(self.stream):
-            for target in self.zero_buffs:
-                target.zero_()
 
 
 gpu_memory_pool = GPUMemoryPool()
